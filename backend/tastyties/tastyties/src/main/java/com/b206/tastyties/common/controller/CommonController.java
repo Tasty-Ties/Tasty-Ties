c1fbaa0279fca903365a369cac1c7697783476da
leejongmin <rjsqktod1@gmail.com>
2024-07-25 09:45:08 +0900

    refactor: @Controller -> @RestController 변경
    controller의 의미를 명확화하기 위해
    어노테이션을 변경했습니다.
    
    현재 상황에서 응답 데이터가 변경되는 점은 없습니다
    
    Signed-off-by: 이종민 <rjsqktod1@gmail.com>

--- backend/tastyties/tastyties/src/main/java/com/b206/tastyties/common/controller/CommonController.java
+++ backend/tastyties/tastyties/src/main/java/com/b206/tastyties/common/controller/CommonController.java
@@ -4,14 +4,14 @@ import com.b206.tastyties.common.dto.CommonResponseDTO;
 import com.b206.tastyties.common.dto.CountryCodeResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
-@Controller
+@RestController
 public class CommonController {
 
     @GetMapping("/countries")
