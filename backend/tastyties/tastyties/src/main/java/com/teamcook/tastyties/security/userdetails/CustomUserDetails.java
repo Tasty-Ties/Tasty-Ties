a10fe30b4cd86ab6c00d3b5ec9dc66e8d0b92e84
leejongmin <rjsqktod1@gmail.com>
2024-07-25 12:03:52 +0900

    refactor: 최상위 폴더 구조 변경
    b206에서 teamcook으로 이름을 변경하였습니다.
    우리가 누구? teamcook....
    
    Signed-off-by: leejongmin <rjsqktod1@gmail.com>

---
+++ backend/tastyties/tastyties/src/main/java/com/teamcook/tastyties/security/userdetails/CustomUserDetails.java
@@ -0,0 +1,33 @@
+package com.teamcook.tastyties.security.userdetails;
+
+
+import com.teamcook.tastyties.user.entity.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+
+public class CustomUserDetails implements UserDetails {
+
+    private User user;
+
+    public CustomUserDetails(User user) {
+        this.user = user;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.emptyList();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+}
---
+++ backend/tastyties/tastyties/src/main/java/com/teamcook/tastyties/user/service/UserService.java
@@ -0,0 +1,48 @@
+package com.teamcook.tastyties.user.service;
+
+import com.teamcook.tastyties.user.dto.UserRegistrationDTO;
+import com.teamcook.tastyties.user.entity.User;
+import com.teamcook.tastyties.user.exception.UserIDAlreadyExistsException;
+import com.teamcook.tastyties.user.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public void registerUser(UserRegistrationDTO request) {
+        String encodedPassword = passwordEncoder.encode(request.getPassword());
+        if (userRepository.existsByUsername(request.getUsername())) {
+            throw new UserIDAlreadyExistsException("이미 존재하는 사용자 ID입니다");
+        }
+        User newUser = new User();
+        newUser.setUsername(request.getUsername());
+        newUser.setPassword(encodedPassword);
+        newUser.setNickname(request.getNickname());
+        newUser.setCountryCode(request.getCountryCode());
+        newUser.setLanguageCode(request.getLanguageCode());
+
+        newUser.setEmail(request.getEmailId() + "@" + request.getEmailDomain());
+        newUser.setBirth(request.getBirth());
+
+        userRepository.save(newUser);
+    }
+
+    public boolean isUserNameAvailable(String username) {
+        return !userRepository.existsByUsername(username);
+    }
+
+    public boolean isNicknameAvailable(String nickname) {
+        return !userRepository.existsByNickname(nickname);
+    }
+}
