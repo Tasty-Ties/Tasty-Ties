d5dbd9ca48a2ea2fb608b20627217fd39360339e
leejongmin <rjsqktod1@gmail.com>
2024-07-25 14:10:01 +0900

    fix: UserExceptionHandler basePackages 변경
    기본 경로를 바꾸면서, basePackages를 같이 변경하지않아
    생기는 문제를 fix 했습니다
    
    Signed-off-by: leejongmin <rjsqktod1@gmail.com>

--- backend/tastyties/tastyties/src/main/java/com/teamcook/tastyties/user/exception/UserExceptionHandler.java
+++ backend/tastyties/tastyties/src/main/java/com/teamcook/tastyties/user/exception/UserExceptionHandler.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice(basePackages = "com.b206.tastyties.user")
+@ControllerAdvice(basePackages = "com.teamcook.tastyties.user")
 public class UserExceptionHandler {
 
     @ExceptionHandler(UserIDAlreadyExistsException.class)
