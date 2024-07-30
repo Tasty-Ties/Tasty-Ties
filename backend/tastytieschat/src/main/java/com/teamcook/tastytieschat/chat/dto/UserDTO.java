d4b082a6db0806ca5b64f881d01eafef4b3d031a
이지원 <wldnjs7851@naver.com>
2024-07-31 08:40:48 +0900

    refactor: Language enum 객체로 언어 관리

--- backend/tastytieschat/src/main/java/com/teamcook/tastytieschat/chat/dto/UserDTO.java
+++ backend/tastytieschat/src/main/java/com/teamcook/tastytieschat/chat/dto/UserDTO.java
@@ -1,5 +1,6 @@
 package com.teamcook.tastytieschat.chat.dto;
 
+import com.teamcook.tastytieschat.chat.constant.Language;
 import com.teamcook.tastytieschat.chat.constant.UserType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -15,7 +16,7 @@ public class UserDTO {
     private int id;
     private UserType type;
     private String nickname;
-    private String language;
+    private Language language;
     private Instant enteredTime;
 
     @Override
