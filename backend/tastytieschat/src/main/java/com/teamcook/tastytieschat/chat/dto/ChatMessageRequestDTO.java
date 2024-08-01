3ef862f1c3cc4ec301e3915b3f4efa4c7edd041e
김세민 <2007ksm@naver.com>
2024-08-01 13:15:42 +0900

    test: GPT 번역 시간 측정 테스트

--- backend/tastytieschat/src/main/java/com/teamcook/tastytieschat/chat/dto/ChatMessageRequestDTO.java
+++ backend/tastytieschat/src/main/java/com/teamcook/tastytieschat/chat/dto/ChatMessageRequestDTO.java
@@ -1,8 +1,10 @@
 package com.teamcook.tastytieschat.chat.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ChatMessageRequestDTO {
 
     private int userId;
