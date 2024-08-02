0a7b3492c0425c76e0953f01dd2488bfcaca024c
이지원 <wldnjs7851@naver.com>
2024-08-02 16:37:53 +0900

    feat: 채팅 내용 조회 추가

--- backend/tastytieschat/src/main/java/com/teamcook/tastytieschat/chat/service/ChatMessageService.java
+++ backend/tastytieschat/src/main/java/com/teamcook/tastytieschat/chat/service/ChatMessageService.java
@@ -3,9 +3,12 @@ package com.teamcook.tastytieschat.chat.service;
 import com.teamcook.tastytieschat.chat.dto.ChatMessageDto;
 import com.teamcook.tastytieschat.chat.entity.ChatMessage;
 
+import java.util.Map;
+
 public interface ChatMessageService {
 
     void createChatMessage(ChatMessage ChatMessage);
     ChatMessageDto getLastChatMessageByChatRoomId(String chatRoomId);
+    Map<String, Object> getChatMessagesByChatRoomId(Map<String, Object> requestParams);
 
 }
