c96eaddbfdf41d54f406eb9df5f8ef243d5f362f
박소정 <thwjd0235@naver.com>
2024-08-03 18:12:16 +0900

    fix: axios인스턴스 생성 수정
    리프레시 토큰으로 엑세스토큰 재발급 받는 과정이 오류가 있어서
    수정했습니다.
    로그아웃 시 메인페이지로 이동하게 했습니다.
    마이페이지 기본정보 불러오는 작업 하는 중에 머지합니다ㅎㅎㅎㅎ
    
    Signed-off-by: 박소정 <thwjd0235@naver.com>

--- frontend/gongtong_front/src/components/MyPage/Category.jsx
+++ frontend/gongtong_front/src/components/MyPage/Category.jsx
@@ -1,11 +1,33 @@
 import { Link } from "react-router-dom";
+import useMyPageStore from "../../store/MyPageStore";
+import { useEffect } from "react";
+import instalogo from "../../assets/MyPage/insta.png";
+import youtubelogo from "../../assets/MyPage/Youtube.png";
 
 const Category = () => {
+  const { informations, fetchInformations } = useMyPageStore();
+
+  useEffect(() => {
+    fetchInformations();
+  }, [fetchInformations]);
+
   return (
     <>
       <div>
         <br />
-        <div>프로필</div>
+        <div>
+          <p>프로필사진</p>
+          <p>이름:{informations.nickname}</p>
+          <p>자기소개:{informations.description}</p>
+          <div className="flex">
+            <p className="flex">
+              <img src={instalogo} alt="인스타로고" /> :
+            </p>
+            <p className="flex">
+              <img src={youtubelogo} alt="유튜브로고" /> :
+            </p>
+          </div>
+        </div>
         <br />
         <ul>
           <li className="text-xl">클래스</li>
@@ -22,6 +44,9 @@ const Category = () => {
         <br />
         <ul>
           <li className="text-xl">나의 활동</li>
+          <li>
+            <Link to="/mypage">내 정보</Link>
+          </li>
           <li>
             <Link to="">마일리지</Link>
           </li>
