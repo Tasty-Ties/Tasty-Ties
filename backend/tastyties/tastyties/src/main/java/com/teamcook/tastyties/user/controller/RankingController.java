d4df7c9d5ae1eae27d7e406114ede2d861d0edb8
leejongmin <rjsqktod1@gmail.com>
2024-08-07 08:49:10 +0900

    feat: 사진 순서 변경 시 메인 이미지도 반영
    Signed-off-by: leejongmin <rjsqktod1@gmail.com>

--- backend/tastyties/tastyties/src/main/java/com/teamcook/tastyties/user/controller/RankingController.java
+++ backend/tastyties/tastyties/src/main/java/com/teamcook/tastyties/user/controller/RankingController.java
@@ -1,14 +1,18 @@
 package com.teamcook.tastyties.user.controller;
 
+import com.teamcook.tastyties.common.dto.CommonResponseDto;
 import com.teamcook.tastyties.user.service.UserRewardsService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ZSetOperations;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Set;
 
 @RestController
 @RequestMapping("/ranking")
+@Slf4j
 public class RankingController {
 
     private final UserRewardsService userRewardsService;
@@ -19,8 +23,15 @@ public class RankingController {
     }
 
     @PostMapping("/add")
-    public void addScore(@RequestParam int userId, @RequestParam double score) {
+    public ResponseEntity<CommonResponseDto> addScore(@RequestParam int userId, @RequestParam double score) {
+        log.debug("test");
         userRewardsService.addScore(userId, score);
+        return ResponseEntity.ok()
+                .body(CommonResponseDto.builder()
+                        .stateCode(200)
+                        .message("마일리지가 정상적으로 적립되었습니다.")
+                        .data(null)
+                        .build());
     }
 
 //    @GetMapping("/top")
---
+++ backend/tastyties/tastyties/src/main/java/com/teamcook/tastyties/user/entity/ActivityPoint.java
@@ -0,0 +1,4 @@
+package com.teamcook.tastyties.user.entity;
+
+public class ActivityPoint {
+}
