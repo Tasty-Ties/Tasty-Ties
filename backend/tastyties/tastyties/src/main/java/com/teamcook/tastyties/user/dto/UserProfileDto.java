7a131d5ac98e3f1607f1a29b38843e1c450a41cc
leejongmin <rjsqktod1@gmail.com>
2024-08-09 16:44:21 +0900

    refactor: front 요구 사항 반영
    userProfileDto에 username추가
    
    Signed-off-by: leejongmin <rjsqktod1@gmail.com>

--- backend/tastyties/tastyties/src/main/java/com/teamcook/tastyties/user/dto/UserProfileDto.java
+++ backend/tastyties/tastyties/src/main/java/com/teamcook/tastyties/user/dto/UserProfileDto.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class UserProfileDto {
     private int userId;
     private String profileImageUrl;
+    private String username;
     private String nickname;
     private String description;
     private CountryResponseDto country;
