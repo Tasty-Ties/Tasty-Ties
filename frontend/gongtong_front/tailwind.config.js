d99e41dba60cd838a8af99b74a6a4e4cd65a9f85
이지원 <wldnjs7851@naver.com>
2024-08-12 00:20:28 +0900

    feat: tailwind material 설정 추가

--- frontend/gongtong_front/tailwind.config.js
+++ frontend/gongtong_front/tailwind.config.js
@@ -1,6 +1,13 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+import withMT from "@material-tailwind/react/utils/withMT";
+
+export default withMT({
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_moduels/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -53,4 +60,4 @@ export default {
     },
   },
   plugins: [],
-};
+});
