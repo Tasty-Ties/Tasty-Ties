8621179747aa3b5aca728e93cde58a6abd4a07d6
박소정 <thwjd0235@naver.com>
2024-08-12 16:50:48 +0900

    수강평 카드 하는중

--- frontend/gongtong_front/src/common/components/ReviewCard.jsx
+++ frontend/gongtong_front/src/common/components/ReviewCard.jsx
@@ -3,55 +3,39 @@ import {
   CardHeader,
   CardBody,
   Typography,
-  Button,
 } from "@material-tailwind/react";
 
 const HorizontalCard = ({ title, comment, date, nickname, image }) => {
   return (
-    <Card className="w-full max-w-[48rem] flex-row">
+    <Card className="w-full max-w-[48rem] h-40 flex-row">
       <CardHeader
         shadow={false}
         floated={false}
-        className="m-0 w-2/5 shrink-0 rounded-r-none"
+        className="m-0 h-32 w-52 shrink-0 rounded-lg"
       >
         <img
           src={image}
           alt="card-image"
-          className="h-full w-full object-cover"
+          className="h-full w-full object-cover object-center"
         />
       </CardHeader>
-      <CardBody>
-        <Typography variant="h4" color="blue-gray" className="mb-2">
+      <CardBody className="overflow-hidden">
+        <Typography variant="h5" color="blue-gray" className="mb-1 truncate">
           {title}
         </Typography>
-        <Typography variant="h6" color="gray" className="mb-4 uppercase">
+        <Typography
+          variant="h6"
+          color="gray"
+          className="mb-1 uppercase truncate"
+        >
           {comment}
         </Typography>
-        <Typography color="gray" className="mb-8 font-normal">
+        <Typography color="gray" className="text-sm font-normal truncate">
           {date}
         </Typography>
-        <Typography color="gray" className="mb-8 font-normal">
+        <Typography color="gray" className="text-sm font-normal truncate">
           {nickname}
         </Typography>
-        {/* <a href="#" className="inline-block">
-              <Button variant="text" className="flex items-center gap-2">
-                Learn More
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke="currentColor"
-                  strokeWidth={2}
-                  className="h-4 w-4"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"
-                  />
-                </svg>
-              </Button>
-            </a> */}
       </CardBody>
     </Card>
   );
