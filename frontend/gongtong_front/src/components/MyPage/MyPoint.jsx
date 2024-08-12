adfd81873329d9af61c8189b3068114d0f4154bf
박소정 <thwjd0235@naver.com>
2024-08-13 02:34:20 +0900

    refactor: 마일리지의 버튼의 달을 배열로 바꿔서 코드 수정

--- frontend/gongtong_front/src/components/MyPage/MyPoint.jsx
+++ frontend/gongtong_front/src/components/MyPage/MyPoint.jsx
@@ -6,6 +6,7 @@ const MyPoint = () => {
   const mileageLogs = useMyPageStore((state) => state.mileageLogs);
   const fetchMileageLogs = useMyPageStore((state) => state.fetchMileageLogs);
   const [month, setMonth] = useState(1); // 기본 1개월
+  const monthArr = [1, 3, 6, 12];
 
   useEffect(() => {
     fetchMileageLogs(month);
@@ -16,10 +17,11 @@ const MyPoint = () => {
     <div>
       <div className="font-bold text-xl mb-3">마일리지</div>
       <div>
-        <button onClick={() => setMonth(1)}>1개월</button>
-        <button onClick={() => setMonth(3)}>3개월</button>
-        <button onClick={() => setMonth(6)}>6개월</button>
-        <button onClick={() => setMonth(12)}>12개월</button>
+        {monthArr.map((month, index) => (
+          <button key={index} onClick={() => setMonth(month)}>
+            {month}개월
+          </button>
+        ))}
       </div>
       <PointTable log={mileageLogs} />
     </div>
