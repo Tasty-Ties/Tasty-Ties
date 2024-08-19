5c73a2c3945dc7f5e1912fd31e1673f35c890f15
김세민 <2007ksm@naver.com>
2024-08-19 16:28:33 +0900

    chore: application.properties 값 .env 환경 변수로 추출
    루트 디렉토리에 .env 파일 생성하고 사용

--- backend/tastyties/src/main/java/com/teamcook/tastyties/TastytiesApplication.java
+++ backend/tastyties/src/main/java/com/teamcook/tastyties/TastytiesApplication.java
@@ -1,17 +1,20 @@
 package com.teamcook.tastyties;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
-import java.util.Arrays;
 
 @SpringBootApplication
 @EnableScheduling
 public class TastytiesApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.configure().load();
+		dotenv.entries().forEach(entry ->
+				System.setProperty(entry.getKey(), entry.getValue())
+		);
 		ConfigurableApplicationContext ctx = SpringApplication.run(TastytiesApplication.class, args);
 	}
 
--- backend/tastytieschat/src/main/java/com/teamcook/tastytieschat/TastytieschatApplication.java
+++ backend/tastytieschat/src/main/java/com/teamcook/tastytieschat/TastytieschatApplication.java
@@ -1,5 +1,6 @@
 package com.teamcook.tastytieschat;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TastytieschatApplication {
 
     public static void main(String[] args) {
+        Dotenv dotenv = Dotenv.configure().load();
+
+        dotenv.entries().forEach(entry ->
+                System.setProperty(entry.getKey(), entry.getValue())
+        );
         SpringApplication.run(TastytieschatApplication.class, args);
     }
 
